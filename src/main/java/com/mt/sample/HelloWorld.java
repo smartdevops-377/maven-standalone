d119a4f5e35f6f2120a5dde7e477d4b3d657c896
smartdevops-377 <technosmart3377@gmail.com>
2025-02-06 06:25:00 +0530

    Update HelloWorld.java

--- src/main/java/com/mt/sample/HelloWorld.java
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello all good day,Hello Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
+		System.out.println("Hello all good day,Hello Welcome to jenkins CI-CD Tool !! Today Date is:  " + getLocalCurrentDate());
 
 	}
 
